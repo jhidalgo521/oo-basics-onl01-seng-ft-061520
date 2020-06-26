960e1e8ff6077b9642c158a7b2f5677b1cede288
Jose Hidago <jhidalgo521@gmail.com>
2020-06-26 16:37:16 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,7 +1,7 @@
 # Make your shoe class here!
 class Shoe 
   
-  attr_accessor :author, :page_count, :genre
+  attr_accessor :brand, :color, :size, :material, :condition
     attr_reader :title
  
     def initialize(brand)
